b162aa38ffe03dc34c54ea4426f14b0a682c2382
PreciseAardvark <abraha.delarosaaguayo.509@gmail.com>
2021-11-24 00:15:53 -0600

    feat: Inlcude

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
 const axios = require("axios");
-const { response } = require('express');
+const { response } = require("express");
+
+require('dotenv').config({path: './.env'});
+const puerto = process.env.PORT;
+
 
 const main = async () => {
   const response = await axios.get("https://pokeapi.co/api/v2/pokemon/pikachu");
@@ -8,26 +12,26 @@ const main = async () => {
     data: { moves },
   } = response;
   let pokeMoves = [];
-  moves.map((movimientos)=>{
+  moves.map((movimientos) => {
     pokeMoves.push(movimientos.move);
-  })
-  return pokeMoves
+  });
+  return pokeMoves;
 };
 
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
+
 
 /* El servidor recibe la solicitud y envia un hola mundo en respuesta*/
 app.get('/', async (req, res) => {
-    const pokemonMoves = await main();
-     res.json({
-      statusCode: 200,
-      message: "request was proceced succesfully",
-      data: pokemonMoves
-     });
+  const pokemonMoves = await main();
+  res.json({
+    statusCode: 200,
+    message: "request was proceced succesfully",
+    data: pokemonMoves,
+  });
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+app.listen(puerto, () => {
+  console.log(`Servidor escuchando en http://localhost:${puerto}`);
 });
-
